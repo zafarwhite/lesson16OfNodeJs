0448e55377a535b7098d2ce13835a4f46702238a
Zafar Kudratov <zafarkudratov@gmail.com>
2019-01-30 19:26:57 +0200

    array

--- file1.js
+++ file1.js
@@ -1,5 +1,8 @@
 var a = 'Hello from file1';
 var b = 'Another hello from file1';
 
-module.exports = a;//'module' -> exports from 'file1' in NodeJs
-module.exports = b;
+var arr = [a, b];
+
+// module.exports.a = a;//'module' -> exports from 'file1' in NodeJs
+// module.exports.b = b;
+module.exports = arr;
