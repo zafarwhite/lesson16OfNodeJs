a62b64cf6904c46ddc395e4aeff94a43c16ac1c8
Zafar Kudratov <zafarkudratov@gmail.com>
2019-01-30 19:30:58 +0200

    user

--- file1.js
+++ file1.js
@@ -1,8 +1,21 @@
 var a = 'Hello from file1';
 var b = 'Another hello from file1';
 
-var arr = [a, b];
+// var arr = [a, b];
 
-// module.exports.a = a;//'module' -> exports from 'file1' in NodeJs
-// module.exports.b = b;
-module.exports = arr;
+// // module.exports.a = a;//'module' -> exports from 'file1' in NodeJs
+// // module.exports.b = b;
+// module.exports = arr;
+
+var user = {
+    id: 4,
+    name: 'Adam'
+};
+
+var user2 = {
+     id: 6,
+     name: 'Ewa'
+};
+
+
+module.exports.u = user;
