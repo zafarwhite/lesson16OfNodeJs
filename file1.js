5d8e94aee853444132673c5c01eda6d81153f694
Zafar Kudratov <zafarkudratov@gmail.com>
2019-01-30 19:42:34 +0200

    import files

--- file1.js
+++ file1.js
@@ -1,21 +1,31 @@
 var a = 'Hello from file1';
-var b = 'Another hello from file1';
-
-// var arr = [a, b];
-
-// // module.exports.a = a;//'module' -> exports from 'file1' in NodeJs
-// // module.exports.b = b;
-// module.exports = arr;
-
-var user = {
-    id: 4,
-    name: 'Adam'
-};
-
-var user2 = {
-     id: 6,
-     name: 'Ewa'
-};
+module.exports = a;
 
 
-module.exports = user;
+
+
+
+
+
+
+// var a = 'Hello from file1';
+// var b = 'Another hello from file1';
+
+// // var arr = [a, b];
+
+// // // module.exports.a = a;//'module' -> exports from 'file1' in NodeJs
+// // // module.exports.b = b;
+// // module.exports = arr;
+
+// var user = {
+//     id: 4,
+//     name: 'Adam'
+// };
+
+// var user2 = {
+//      id: 6,
+//      name: 'Ewa'
+// };
+
+
+// module.exports = user;
